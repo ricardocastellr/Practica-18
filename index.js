c8e9055bfcac7fb28d01681c17f559420ca9965a
ricardocastellr <rcastell@ucol.mx>
2023-04-18 08:33:00 -0600

    Comentarios

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require('express'); // Requerimos express
+const mongoose = require('mongoose'); // Requerimos mongoose
+require('dotenv').config(); // Requerimos dotenv y lo iniciamos
 
 const app = express();
 const port = process.env.PORT || 9000;
 
 //routes
 app.get("/", (req, res) =>{
-    res.send("Welcome to my API");
+    res.send("Welcome to my API"); // Mensaje al entrar a la raiz
 })
 
 mongoose
     .connect(process.env.MONGODB_URI)
-    .then(() => console.log('Connected to MongoDB Atlas'))
+    .then(() => console.log('Connected to MongoDB Atlas')) // Conexión correcta a MongoDB Atlas
     .catch((error) => console.error(error))
 
-app.listen(port, () => console.log("Server listening on port", port));
+app.listen(port, () => console.log("Server listening on port", port)); // El puerto en el cual funciona
 
 
 
